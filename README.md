c6b77b2cb4ec2a9726d376ac399630ae38a96bb5
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-10-18 11:00:34 +0200

    Introduce first usage example

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Rust bindings for CrowdStrike Falcon API.
 
 > falcon-rs is an open source project, not a CrowdStrike product. As such, it carries no formal support, expressed or implied.
 
+
+### Examples
+
+Run example with
+```
+cargo run --release --example simple
+```
---
+++ examples/simple.rs
@@ -0,0 +1,37 @@
+use crowdstrike_falcon::apis::configuration::{Configuration};
+use crowdstrike_falcon::apis::oauth2_api::{oauth2_access_token};
+use crowdstrike_falcon::apis::incidents_api::{crowd_score};
+
+use std::env;
+
+#[tokio::main]
+async fn main() {
+    let falcon_client_id = env::var("FALCON_CLIENT_ID")
+        .expect("Missing FALCON_CLIENT_ID environment variable. Please provide your OAuth2 API Client ID for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys.");
+    let falcon_client_secret = env::var("FALCON_CLIENT_SECRET")
+        .expect("Missing FALCON_CLIENT_SECRET environment variable. Please provide your OAuth2 API Client Secret for authentication with CrowdStrike Falcon platform. Establishing and retrieving OAuth2 API credentials can be performed at https://falcon.crowdstrike.com/support/api-clients-and-keys.");
+
+    let mut configuration = Configuration {
+        ..Default::default()
+    };
+    let response = oauth2_access_token(&configuration, &falcon_client_id, &falcon_client_secret, None)
+        .await
+        .expect("Could not authenticate with CrowdStrike API");
+
+    configuration.oauth_access_token = Some(response.access_token);
+
+    let crowd_score_response = crowd_score(&configuration, None, None, None, None)
+        .await
+        .expect("Could not fetch CrowdScore");
+
+    if ! crowd_score_response.errors.is_empty() {
+        eprintln!("Errors occured while calculating CrowdScore: {:?}", crowd_score_response.errors);
+    }
+
+    if crowd_score_response.resources.is_empty() {
+        eprintln!("No CrowdScore returned")
+    }
+
+    let score = crowd_score_response.resources.last().unwrap();
+    println!("As of {} your CrowdScore is {}.", score.timestamp, score.score)
+}
