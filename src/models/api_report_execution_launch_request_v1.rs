2af64578d246a0c0db5318d853359f031da4af25
Simon Lukasik <simon.lukasik@crowdstrike.com>
2021-11-01 13:04:42 +0100

    Re-generate the codebase using the latest swagger

--- src/models/api_report_execution_launch_request_v1.rs
+++ src/models/api_report_execution_launch_request_v1.rs
@@ -9,44 +9,20 @@
  */
 
 
-use reqwest;
 
-#[derive(Debug, Clone)]
-pub struct Configuration {
-    pub base_path: String,
-    pub user_agent: Option<String>,
-    pub client: reqwest::Client,
-    pub basic_auth: Option<BasicAuth>,
-    pub oauth_access_token: Option<String>,
-    pub bearer_access_token: Option<String>,
-    pub api_key: Option<ApiKey>,
-    // TODO: take an oauth2 token source, similar to the go one
+
+#[derive(Clone, Debug, PartialEq, Default, Serialize, Deserialize)]
+pub struct ApiReportExecutionLaunchRequestV1 {
+    #[serde(rename = "id")]
+    pub id: String,
 }
 
-pub type BasicAuth = (String, Option<String>);
-
-#[derive(Debug, Clone)]
-pub struct ApiKey {
-    pub prefix: Option<String>,
-    pub key: String,
-}
-
-impl Configuration {
-    pub fn new() -> Configuration {
-        Configuration::default()
-    }
-}
-
-impl Default for Configuration {
-    fn default() -> Self {
-        Configuration {
-            base_path: "https://api.crowdstrike.com".to_owned(),
-            user_agent: Some("rusty_falcon/0.0.1".to_owned()),
-            client: reqwest::Client::new(),
-            basic_auth: None,
-            oauth_access_token: None,
-            bearer_access_token: None,
-            api_key: None,
+impl ApiReportExecutionLaunchRequestV1 {
+    pub fn new(id: String) -> ApiReportExecutionLaunchRequestV1 {
+        ApiReportExecutionLaunchRequestV1 {
+            id,
         }
     }
 }
+
+
